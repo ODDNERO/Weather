b0d193989aa56080224c37bc14ea20f813b2eea5
NERO <mxzxro@gmail.com>
2024-07-11 22:48:17 +0900

    🩹 [APIRouter] 불필요하게 Class로 감싸지 않도록 수정

--- Weather/Network/APIRouter.swift
+++ Weather/Network/APIRouter.swift
@@ -8,36 +8,36 @@
 import Alamofire
 import Foundation
 
-final class APIRouter {
-    enum WeatherRequest {
-        case current(cityID: Int)
-        case forecast(cityID: Int)
-        
-        private var baseURL: String {
-            return "https://api.openweathermap.org/data/"
+
+enum APIRouter {
+    case current(cityID: Int)
+    case forecast(cityID: Int)
+    
+    private var baseURL: String {
+        return "https://api.openweathermap.org/data/"
+    }
+    private var APIVersion: String {
+        return "2.5/"
+    }
+    
+    var endpoint: URL {
+        switch self {
+        case .current:
+            URL(string: baseURL + APIVersion + "weather")!
+        case .forecast:
+            URL(string: baseURL + APIVersion + "forecast")!
         }
-        private var APIVersion: String {
-            return "2.5/"
-        }
-        
-        var endpoint: URL {
-            switch self {
-            case .current:
-                URL(string: baseURL + APIVersion + "weather")!
-            case .forecast:
-                URL(string: baseURL + APIVersion + "forecast")!
-            }
-        }
-        
-        var method: HTTPMethod {
-            return .get
-        }
-        
-        var parameter: Parameters {
-            switch self {
-            case .current(cityID: let cityID), .forecast(cityID: let cityID):
-                ["id": cityID, "appid": WeatherAPI.key, "lang": "kr"]
-            }
+    }
+    
+    var method: HTTPMethod {
+        return .get
+    }
+    
+    var parameter: Parameters {
+        switch self {
+        case .current(cityID: let cityID), .forecast(cityID: let cityID):
+            ["id": cityID, "appid": WeatherAPI.key, "lang": "kr"]
         }
     }
 }
+
