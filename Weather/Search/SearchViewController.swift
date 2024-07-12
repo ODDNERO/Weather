20004ab378515b6663d47463e93bd82f19477fad
NERO <mxzxro@gmail.com>
2024-07-12 18:51:10 +0900

    💄 [SearchVC] ToolBar 숨김 처리 / NavigationBar Button 추가 (Menu Button 임시 구현)

--- Weather/Search/SearchViewController.swift
+++ Weather/Search/SearchViewController.swift
@@ -17,4 +17,32 @@ final class SearchViewController: BaseViewController<SearchView> {
         super.viewWillAppear(animated)
         self.contentView.setupBackgroundColor()
     }
+    
+    override func setupNavigationBar() {
+        navigationController?.navigationBar.isHidden = false
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIBox.Icon.back, style: .plain, target: self, action: #selector(backButtonClicked))
+        navigationItem.rightBarButtonItem = setupMenuButton()
+    }
+    
+    override func setupToolBar() {
+        navigationController?.toolbar.isHidden = true
+    }
+}
+
+extension SearchViewController {
+    @objc private func backButtonClicked() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private func setupMenuButton() -> UIBarButtonItem {
+        let oneAction = UIAction(title: "임시 1", image: nil) { _ in
+            //
+        }
+        let twoAction = UIAction(title: "임시 2", image: nil) { _ in
+            //
+        }
+        let menu = UIMenu(children: [oneAction, twoAction])
+        let menuButton = UIBarButtonItem(title: nil, image: UIBox.Icon.menu, target: nil, action: nil, menu: menu)
+        return menuButton
+    }
 }
