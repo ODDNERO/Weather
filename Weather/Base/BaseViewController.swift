91e94e06b194a2bb4109bfd41851c6ef5d972ef3
NERO <mxzxro@gmail.com>
2024-07-12 18:00:29 +0900

    💄 [BaseVC] NavigationBar/ToolBar 설정

--- Weather/Base/BaseViewController.swift
+++ Weather/Base/BaseViewController.swift
@@ -16,13 +16,31 @@ class BaseViewController<ContentView: UIView>: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupNavigationBar()
         addEventHandler()
         setupDelegate()
         bindData()
     }
     
-    func setupNavigationBar() {}
+    override func viewWillAppear(_ animated: Bool) {
+        setupNavigationBar()
+        setupToolBar()
+    }
+    
+    func setupNavigationBar() {
+        navigationController?.navigationBar.tintColor = .white
+        navigationItem.backButtonTitle = ""
+    }
+    func setupToolBar() {
+        let appearance = UIToolbarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = contentView.backgroundColor?.withAlphaComponent(0.8) //임시
+        appearance.shadowColor = .white.withAlphaComponent(0.5)
+        navigationController?.toolbar.scrollEdgeAppearance = appearance
+        navigationController?.toolbar.standardAppearance = appearance
+        navigationController?.toolbar.tintColor = .white
+        navigationController?.isToolbarHidden = false
+    }
+    
     func addEventHandler() {}
     func setupDelegate() {}
     func bindData() {}
