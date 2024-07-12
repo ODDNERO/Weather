f7db53b1c052c017cbe35810fb411b8dabd259d9
NERO <mxzxro@gmail.com>
2024-07-12 17:47:53 +0900

    💄 UIBox [Icon] UIImage Enum 생성

---
+++ Weather/Resource/UIBox/Icon.swift
@@ -0,0 +1,17 @@
+//
+//  Icon.swift
+//  Weather
+//
+//  Created by NERO on 7/12/24.
+//
+
+import UIKit
+
+enum UIBox {
+    enum Icon {
+        static let map = UIImage(systemName: "map")
+        static let list = UIImage(systemName: "list.bullet")
+        static let back = UIImage(systemName: "chevron.left")
+        static let menu = UIImage(systemName: "filemenu.and.selection")
+    }
+}
