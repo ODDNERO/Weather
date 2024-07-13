475544c614a6d19c2bd6d12b6a67a77204cf4af8
NERO <mxzxro@gmail.com>
2024-07-14 06:49:21 +0900

    ✅ Forecase Weather API 통신 확인 완료

--- Weather/Main/WeatherViewModel.swift
+++ Weather/Main/WeatherViewModel.swift
@@ -20,18 +20,28 @@ extension WeatherViewModel {
     private func transformData() {
         inputCityID.bind { cityID in
             self.requestCurrentWeather(cityID)
+            self.requestForecaseWeather(cityID)
         }
     }
     
     private func requestCurrentWeather(_ id: Int) {
         NetworkManager.requestAPI(APIRouter.current(cityID: id)) { (weather: CurrentWeather?) in
-            if let weather,
-               let weatherInfo = weather.weather.first,
+            if let weather, let weatherInfo = weather.weather.first,
                let weatherCondition = WeatherCondition.convertCase(condition: weatherInfo.main) {
                 self.outputViewColor.value = weatherCondition.color
-                print(weatherCondition)
+                print(weather, "\n")
             } else {
-                //
+                self.outputViewColor.value = .red
+                print(weather, "\n")
+            }
+        }
+    }
+    
+    private func requestForecaseWeather(_ id: Int) {
+        NetworkManager.requestAPI(APIRouter.forecast(cityID: id)) { (weather: ForecaseWeather?) in
+            if let weather, let weatherInfo = weather.list.first {
+                print(">>>>>", Date())
+                print(weather)
             }
         }
     }
