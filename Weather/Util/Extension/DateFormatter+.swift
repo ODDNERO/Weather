716454f57190fdb7266d82f5e274e2edb37222f9
NERO <mxzxro@gmail.com>
2024-07-15 01:28:41 +0900

    ✨ [DateFormatter+] 각 Country/State Case에 따른 Date 반환 메서드 구현
    ! 추후 하나의 함수에서 처리할 수 있도록 리팩토링 필요

---
+++ Weather/Util/Extension/DateFormatter+.swift
@@ -0,0 +1,70 @@
+//
+//  DateFormatter+.swift
+//  Weather
+//
+//  Created by NERO on 7/14/24.
+//
+
+import Foundation
+
+extension DateFormatter {
+    static func convertKRDate(_ date: String) -> Date? {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(identifier: "UTC")
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        guard let UTCDate = formatter.date(from: date) else { return nil }
+        
+        let KSTTimeZone = TimeZone(identifier: "Asia/Seoul")!
+        let KSTSeconds = KSTTimeZone.secondsFromGMT(for: UTCDate)
+        let KRDate = UTCDate.addingTimeInterval(TimeInterval(KSTSeconds))
+        return KRDate
+    }
+    
+    static func convertTXDate(_ date: String) -> Date? {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(identifier: "UTC")
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        guard let UTCDate = formatter.date(from: date) else { return nil }
+        
+        let CSTTimeZone = TimeZone(identifier: "America/Chicago")!
+        let CSTSeconds = CSTTimeZone.secondsFromGMT(for: UTCDate)
+        let TXDate = UTCDate.addingTimeInterval(TimeInterval(CSTSeconds))
+        return TXDate
+    }
+    
+    static func convertCADate(_ date: String) -> Date? {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(identifier: "UTC")
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        guard let UTCDate = formatter.date(from: date) else { return nil }
+        
+        let PSTTimeZone = TimeZone(identifier: "America/Los_Angeles")!
+        let PSTSeconds = PSTTimeZone.secondsFromGMT(for: UTCDate)
+        let CADate = UTCDate.addingTimeInterval(TimeInterval(PSTSeconds))
+        return CADate
+    }
+    
+    static func convertHKDate(_ date: String) -> Date? {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(identifier: "UTC")
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        guard let UTCDate = formatter.date(from: date) else { return nil }
+        
+        let HKTTimeZone = TimeZone(identifier: "Asia/Hong_Kong")!
+        let HKTSeconds = HKTTimeZone.secondsFromGMT(for: UTCDate)
+        let HKDate = UTCDate.addingTimeInterval(TimeInterval(HKTSeconds))
+        return HKDate
+    }
+    
+    static func convertJPDate(_ date: String) -> Date? {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(identifier: "UTC")
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        guard let UTCDate = formatter.date(from: date) else { return nil }
+        
+        let JSTTimeZone = TimeZone(identifier: "Asia/Tokyo")!
+        let JSTSeconds = JSTTimeZone.secondsFromGMT(for: UTCDate)
+        let JPDate = UTCDate.addingTimeInterval(TimeInterval(JSTSeconds))
+        return JPDate
+    }
+}
