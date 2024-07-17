13c397348bd3a64b78be8d256857a13e60c8ecf4
NERO <mxzxro@gmail.com>
2024-07-17 21:38:27 +0900

    💄 [UIBox] Icon 추가

--- Weather/Resource/UIBox/Icon.swift
+++ Weather/Resource/UIBox/Icon.swift
@@ -13,5 +13,11 @@ enum UIBox {
         static let list = UIImage(systemName: "list.bullet")
         static let back = UIImage(systemName: "chevron.left")
         static let menu = UIImage(systemName: "filemenu.and.selection")
+        
+        static let wind = UIImage(systemName: "wind")
+        static let cloud = UIImage(systemName: "drop.fill")
+        static let thermometer = UIImage(systemName: "thermometer.medium")
+        static let humidity = UIImage(systemName: "humidity")
+        static let earth = UIImage(systemName: "globe.americas.fill")
     }
 }
